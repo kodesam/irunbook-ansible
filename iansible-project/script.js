1d195b45629e2d5812eb93cd53a2321501b13638
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-11-14 22:20:44 +0100

    Create script.js

---
+++ iansible-project/script.js
@@ -0,0 +1,27 @@
+// Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+const apiKey = 'YOUR_OPENAI_API_KEY';
+const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+async function generateResponse() {
+    const inputText = document.getElementById('inputText').value;
+
+    // Make a POST request to OpenAI API
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            prompt: inputText,
+            max_tokens: 100
+            // You can customize other parameters as needed
+        })
+    });
+
+    // Parse the JSON response
+    const responseData = await response.json();
+
+    // Display the generated text in the outputText div
+    document.getElementById('outputText').innerText = responseData.choices[0].text;
+}
